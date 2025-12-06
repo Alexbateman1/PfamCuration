c9d3f5a5bdec5317a4a10a40700b120cf5409e53
Alex Bateman <agb@ebi.ac.uk>
2025-12-06 12:28:33 +0000

    minor update

--- SCRIPTS/add_annotation.sh
+++ SCRIPTS/add_annotation.sh
@@ -3,7 +3,7 @@ echo $1
 
 species_summary_new.pl $1
 
-cd $1; add_abb_ref.py; add_pdb_ref.py; add_swiss_ref.pl; query_paperblast.py; ted_ali.pl SEED; cd $current_dir
+cd $1; add_abb_ref.py; add_pdb_ref.py; add_swiss_ref.pl; swissprot.pl -n 20; query_paperblast.py; ted_ali.pl SEED; cd $current_dir
 
 add_images.py $1
 add_foldseek.py $1
